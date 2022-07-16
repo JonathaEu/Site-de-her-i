c1cc1406ea9e5a38faa2caef365d6e535a9ec1d0
Jonatha Serpa <jonathaserpa20@gmail.com>
2022-07-16 17:14:23 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+function Home() {
+    return <div>Home</div>
+}
+
+export default Home
