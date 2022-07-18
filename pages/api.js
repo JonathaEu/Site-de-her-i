e289bc777d7561bfd5bb3d7984ebd61f7cd27bcd
Jonatha Serpa <jonathaserpa20@gmail.com>
2022-07-17 22:52:34 -0300

    modified:   pages/api.js commit

--- pages/api.js
+++ pages/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'gateway.marvel.com:443/v1/public/characters?apikey=73a123efb948c2d57d2387712f194cf8'
+    baseURL: 'https://gateway.marvel.com:443/v1/public/characters?apikey='
 });
 
 export default api;
